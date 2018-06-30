b4561cc77c9c9b06e174485197b12d11ce830979
mrabalogu <chineduabalogu@yahoo.com>
2018-06-30 13:55:04 +0100

    logo added

--- config/routes.rb
+++ config/routes.rb
@@ -18,6 +18,7 @@ Rails.application.routes.draw do
   resources :reports
   resources :report_categories
   devise_for :users
+  resources :users 
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
